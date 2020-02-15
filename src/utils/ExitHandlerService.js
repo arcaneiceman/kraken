2fd5fca2dcc3f93e78c9ad4bb3dd36a1260f052e
wali.usmani <wali.usmani@aequilibrium.ca>
2020-02-15 00:52:18 -0800

    Needed another fix

--- src/utils/ExitHandlerService.js
+++ src/utils/ExitHandlerService.js
@@ -5,16 +5,16 @@ if (window.require) {
 }
 
 const handleExit = async (event) => {
+    event.returnValue = false
     if (isElectron()) {
         let result = await electron.remote.dialog.showMessageBox({
             message: 'The worker is still running. Do you still want to quit?',
             buttons: ['Yes', 'No']
         })
-        if (result.response === 0)
+        if (result.response === 0) { // Yes 
+            window.removeEventListener("beforeunload", handleExit)
             electron.remote.getCurrentWindow().close()
-    }
-    else{
-        event.returnValue = false
+        }
     }
 }
 
